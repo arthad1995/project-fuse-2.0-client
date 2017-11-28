b8fed1209a2bb8769b41ee5f40bcc53653e410e7
tofurama3000 <tofurama3000@hotmail.com>
2017-11-27 21:18:03 -0700

    Added compression to production server

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+var compression = require('compression')
 
+app.use(compression())
 app.use(express.static('dist'))
 
 app.listen(8081, () => console.log('Example app listening on port 8081!'))
