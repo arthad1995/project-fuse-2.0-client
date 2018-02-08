5415db371b48c25c3d604885ab720ea4fa5459a2
matttolman <tofurama3000@users.noreply.github.com>
2018-02-07 22:32:53 -0700

    Improved top search bar

--- src/components/layout/search.jsx
+++ src/components/layout/search.jsx
@@ -6,10 +6,14 @@ export default class SearchBar extends Component {
 
     render() {
         return <div className={(this.props.noBlue? 'search-bar-embeded-menu' :"search-bar-menu tone1-5-color ") + (this.props.className || '')}>
-            <SearchInput value={this.props.value} className="search-input" onChange={this.props.searchCallback} />
-            {!this.props.noBlue? <div onClick={this.props.buttonCallback} className="btn tone1-4-color">
-                <i className="fas fa-angle-up"></i>
-            </div> : ''}
+            <div className="search-bar-menu__content">
+                <i className="search-icon fas fa-search" />
+                <SearchInput value={this.props.value} className="search-input" onChange={this.props.searchCallback}>
+                </SearchInput>
+                {!this.props.noBlue? <div onClick={this.props.buttonCallback} className="btn tone1-4-color">
+                    <i className="fas fa-angle-up"></i>
+                </div> : ''}
+            </div>
         </div>
     }
 }
