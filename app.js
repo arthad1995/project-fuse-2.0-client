c371801e193f4709762167cd0bc8ee360cd28932
matttolman <tofurama3000@users.noreply.github.com>
2018-02-24 11:41:26 -0700

    added favicon

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const compression = require('compression')
 const clusterStability = require('express-cluster-stability')
+const favicon = require('serve-favicon')
+const path = require('path')
 
 clusterStability(({log}) => {
     app.use(compression())
+
+    app.use(favicon(path.join(__dirname,'dist','assets','images','favicon.ico')))
     app.use(express.static('dist'))
-    
-    app.listen(8081, () => log('Example app listening on port 8081!'))    
+
+    app.listen(8081, () => log('Example app listening on port 8081!'))
 })
